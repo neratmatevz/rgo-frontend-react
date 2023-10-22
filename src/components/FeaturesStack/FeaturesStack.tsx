0b9673ffeb6b2e3002a695d93c7eb511fb93f4a2
neratmatevz <matevz.nerat@gmail.com>
2023-10-22 19:55:36 +0200

    izgled

--- src/components/FeaturesStack/FeaturesStack.tsx
+++ src/components/FeaturesStack/FeaturesStack.tsx
@@ -1,41 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./FeaturesStack.css";
 
 const FeaturesStack = () => {
+    
 
-    return(
-    <div>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
-                <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+    return (
+        <div >
+            <div className="row featurette" >
+                <div className="col-md-7">
+                    <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
+                    <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+                </div>
+                <div className="col-md-5">
+                    <img src="https://www.hallsintl.com/image/cache/catalog/g-pro-6-a-gas-500x500.jpg" alt="" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} />
+                </div>
             </div>
-            <div className="col-md-5">
-                <img src="https://www.hallsintl.com/image/cache/catalog/g-pro-6-a-gas-500x500.jpg" alt="" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} />
+            <hr className="featurette-divider " />
+            <div className="row featurette ">
+                <div className="col-md-7 order-md-2">
+                    <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
+                    <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+                </div>
+                <div className="col-md-5 order-md-1 ">
+                    <img src="https://www.hallsintl.com/image/cache/catalog/g-pro-16-ac-gas-500x500.jpg" alt="" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} />
+                </div>
             </div>
-        </div>
-        <hr className="featurette-divider" />
-        <div className="row featurette">
-            <div className="col-md-7 order-md-2">
-                <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
-                <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-            </div>
-            <div className="col-md-5 order-md-1">
-                <img src="https://www.hallsintl.com/image/cache/catalog/g-pro-16-ac-gas-500x500.jpg" alt="" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} />
-            </div>
-        </div>
-        <hr className="featurette-divider" />
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
-                <p className="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
-            </div>
-            <div className="col-md-5">
-                <img src="https://www.fines.si/wp-content/uploads/2017/01/pro-cook-f1-frontal-5.png" alt="" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} />
+            <hr className="featurette-divider " />
+            <div className="row featurette ">
+                <div className="col-md-7">
+                    <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
+                    <p className="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+                </div>
+                <div className="col-md-5">
+                    <img src="https://www.fines.si/wp-content/uploads/2017/01/pro-cook-f1-frontal-5.png" alt="" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} />
 
+                </div>
             </div>
+            <hr className="featurette-divider " />
         </div>
-        <hr className="featurette-divider" />
-    </div>
     );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,25 +8,25 @@ const Header = () => {
 
     return (
         <header >
-            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark" >
+            <nav className="navbar navbar-expand-md navbar-light fixed-top bg-transparent " >
                 <div className="container-fluid ">
-                    <Link to={"/home"} className="navbar-brand">RGO</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <Link to={"/home"} className="navbar-brand"><strong>RGO</strong></Link>
+                    <button className="navbar-toggler border border-dark  border-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                     </button>
-                    <div className="justify-content-center  navbar-collapse collapse " style={{height:60}} aria-expanded={expanded} id="navbarCollapse">
+                    <div className="justify-content-center  navbar-collapse collapse  " style={{height:60}} aria-expanded={expanded} id="navbarCollapse">
                         <ul className="navbar-nav mb-2 mb-md-0 ">
                             <li className="nav-item m-1 headerItem" >
-                                <Link to={"/home"} className="nav-link fs-6  text-white onClickMenu" >DOMOV</Link>
+                                <Link to={"/home"} className="nav-link fs-6  text-dark onClickMenu" ><strong>DOMOV</strong></Link>
                             </li>
                             <li className="nav-item m-1 headerItem " >
-                                <Link to={"/products"} className="nav-link fs-6  text-white onClickMenu" >GOSTINSKA OPREMA</Link>
+                                <Link to={"/products"} className="nav-link fs-6  text-dark onClickMenu" ><strong>GOSTINSKA OPREMA</strong></Link>
                             </li>
                             <li className="nav-item m-1 headerItem">
-                                <Link to={"/aboutus"} className="nav-link fs-6  text-white onClickMenu" >O NAS</Link>
+                                <Link to={"/aboutus"} className="nav-link fs-6  text-dark onClickMenu" ><strong>O NAS</strong></Link>
                             </li>
                             <li className="nav-item m-1 headerItem">
-                                <Link to={"/contact"} className="nav-link fs-6  text-white onClickMenu" >KONTAKT</Link>
+                                <Link to={"/contact"} className="nav-link fs-6 text-dark  onClickMenu" ><strong>KONTAKT</strong></Link>
                             </li>
                             
                         </ul>
