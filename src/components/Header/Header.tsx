e8b19eaade88d407b89ae10aaeb753f0478d4063
matevz.nerat <matevz.nerat@gmail.com>
2023-10-23 18:31:25 +0200

    logo in navbar bg

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
 
     return (
         <header >
-            <nav className="navbar navbar-expand-md navbar-light fixed-top bg-transparent " >
+            <nav className="navbar navbar-expand-md navbar-light fixed-top " >
                 <div className="container-fluid ">
-                    <Link to={"/home"} className="navbar-brand"><strong>RGO</strong></Link>
+                    <Link to={"/home"} className="navbar-brand"><img src="https://mir-s3-cdn-cf.behance.net/user/50/8b64f8478156865.63de953817c5c.png" alt="logo" /></Link>
                     <button className="navbar-toggler border border-dark  border-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                     </button>
